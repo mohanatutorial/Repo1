f2884177c8d494a0336a9870624812163f76d229
mohanatutorial <mohanadharshini573@gmail.com>
2025-07-05 19:35:12 +0530

    Added comment in the program

--- java project1 mona/dinowin.java
+++ java project1 mona/dinowin.java
@@ -1,6 +1,10 @@
-import java.util.Scanner;
-public class dinowin{
-	public static void main(String args[]){
+/*This game contains four levels.
+Each player get a dino coin to play the game.
+A six-sided die is given to each player if the players get number 1 on throwing the die,they entered into the first level.*/
+
+import java.util.Scanner;												
+public class dinowin{														  
+	public static void main(String args[]){									  
 		System.out.println("WELCOME TO THE GAME");
 		for(int i=1;i<=4;++i){
 			Scanner sc=new Scanner(System.in);
@@ -18,7 +22,8 @@ public class dinowin{
 	}
 
 }
-
+/*If the player entered into the level 1,he has given 5 chances to get a number 5 from the die throw.otherwise,
+he is eliminated.It is a luck based level.After the completion of the level 1,he entered into the level 2.*/ 
 
  class level1{
 	public static void level(){
@@ -54,6 +59,8 @@ public class dinowin{
 	level2.level();
 	}	
 }
+/*In this level,Four hammers punch twice in a minute,the players have to escape from the punch and cross the  white line to finish.
+They should complete this level within 5 minutes.(logic->Each hammer punches 10 times in 5 minutes and 30 sec once it punch in 60 sec)*/
 
 class level2{
 	public static void level(){
@@ -93,11 +100,12 @@ class level2{
 	}
 }
 
-
+/*In this level 3,each player should choose a shape(paper folded) in the options and create any object related to sea in 3 min.
+The players cannot choose the repeated shapes.*/
 class level3{
 	public  void level(){
 		Scanner sc=new Scanner(System.in);
-		System.out.print("The number of players move on to the level 3:");
+		System.out.print("The number of players move on to the level 3 and crossed the white line of the level 2:");
 		int size=sc.nextInt();
 		int[] numbers=new int[size];
 		System.out.println("The players move on to the level 3:");
@@ -153,7 +161,9 @@ class level3{
 
 	
 }
-
+/*Level 4 is a mind game.Alao known as memory game.The players see the pictures of fruits in the screen and after 25 sec,the screen becomes blank.
+If the players correctly guessed the number of fruits that they seen in the screen within 20 sec.
+He is announced as the Dino Winner!*/
 
 class level4{
 	public static  void level(){
